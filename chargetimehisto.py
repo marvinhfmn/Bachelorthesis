fa0c33d9a892b45ff8d4d7e7d87ad7fff977c247
Marvin Hoffmann <marvin.c.hoffmann@gmail.com>
2024-05-14 14:52:51 +0200

    reverted bla variable

--- chargetimehisto.py
+++ chargetimehisto.py
@@ -17,8 +17,6 @@ import warnings
 import tables as pytables
 from pathlib import Path
 
-bla = 1
-
 all_files = glob("/home/wecapstor3/capn/capn106h/sim_databases/*/*/*.db")
 corsika_files = glob("/home/wecapstor3/capn/capn106h/sim_databases/22615/*/*.db")
 nugen_files = [file for file in all_files if file not in corsika_files]
