a490561e5d3123ebf7dacb4d400bd1d59347471e
Marvin Hoffmann <marvin.c.hoffmann@gmail.com>
2024-07-04 14:28:33 +0200

    initial commit, modified SQLDataset class to support custom training label and internally turn NULL entries in database to 0 for computation reasons

---
+++ programming_GNNs_and_training/CustomSQL_dataset.py
@@ -0,0 +1,155 @@
+"""`Dataset` class(es) for reading data from SQLite databases."""
+
+from typing import Any, List, Optional, Tuple, Union, Dict
+import pandas as pd
+import sqlite3
+import numpy as np
+
+from graphnet.data.dataset.dataset import Dataset, ColumnMissingException
+
+
+class CustomSQLiteDataset(Dataset):
+    """Pytorch dataset for reading data from SQLite databases. Also writes NULL entries from database into graph as '0'"""
+
+    # Implementing abstract method(s)
+    def _init(self) -> None:
+        # Check(s)
+        self._database_list: Optional[List[str]]
+        if isinstance(self._path, list):
+            self._database_list = self._path
+            self._all_connections_established = False
+            self._all_connections: List[sqlite3.Connection] = []
+        else:
+            self._database_list = None
+            assert isinstance(self._path, str)
+            assert self._path.endswith(
+                ".db"
+            ), f"Format of input file `{self._path}` is not supported."
+
+        if self._database_list is not None:
+            self._current_database: Optional[int] = None
+
+        # Set custom member variable(s)
+        self._features_string = ", ".join(self._features)
+        self._truth_string = ", ".join(self._truth)
+        if self._node_truth:
+            self._node_truth_string = ", ".join(self._node_truth)
+
+        self._conn: Optional[sqlite3.Connection] = None
+
+    def _post_init(self) -> None:
+        # Purely internal member variables
+        self._missing_variables: Dict[str, List[str]] = {}
+        self._remove_missing_columns()
+        self._close_connection()
+
+    def query_table(
+        self,
+        table: str,
+        columns: Union[List[str], str],
+        sequential_index: Optional[int] = None,
+        selection: Optional[str] = None,
+    ) -> List[Tuple[Any, ...]]:
+        """Query table at a specific index, optionally with some selection."""
+        # Check(s)
+        if isinstance(columns, list):
+            columns = ", ".join(columns)
+            # columns = ", ".join([f"IFNULL({col}, 0) AS {col}" for col in columns])
+
+        if not selection:  # I.e., `None` or `""`
+            selection = "1=1"  # Identically true, to select all
+
+        index = self._get_event_index(sequential_index)
+
+        # Query table
+        assert index is not None
+        self._establish_connection(index)
+        try:
+            assert self._conn
+            if sequential_index is None:
+                combined_selections = selection
+            else:
+                combined_selections = (
+                    f"{self._index_column} = {index} and {selection}"
+                )
+
+            result = self._conn.execute(
+                f"SELECT {columns} FROM {table} WHERE "
+                f"{combined_selections}"
+            ).fetchall()
+        except sqlite3.OperationalError as e:
+            if "no such column" in str(e):
+                raise ColumnMissingException(str(e))
+            else:
+                raise e
+        result = np.nan_to_num(np.asarray(result, dtype=float))
+        return result
+
+    def _get_all_indices(self) -> List[int]:
+        self._establish_connection(0)
+        indices = pd.read_sql_query(
+            f"SELECT {self._index_column} FROM {self._truth_table}", self._conn
+        )
+        self._close_connection()
+        return indices.values.ravel().tolist()
+
+    def _get_event_index(self, sequential_index: Optional[int]) -> int:
+        index: int = 0
+        if sequential_index is not None:
+            index_ = self._indices[sequential_index]
+            if self._database_list is None:
+                if not isinstance(index_, int):
+                    index_ = int(index_)  # type: ignore
+            else:
+                if not isinstance(index_, int):
+                    index_ = int(index_[0])
+            assert isinstance(index_, int)
+            index = index_
+        return index
+
+    # Custom, internal method(s)
+    # @TODO: Is it necessary to return anything here?
+    def _establish_connection(self, i: int) -> "CustomSQLiteDataset":
+        """Make sure that a sqlite3 connection is open."""
+        if self._database_list is None:
+            assert isinstance(self._path, str)
+            if self._conn is None:
+                self._conn = sqlite3.connect(self._path)
+        else:
+            indices = self._indices[i]
+            assert isinstance(indices, list)
+            if self._conn is None:
+                if self._all_connections_established is False:
+                    self._all_connections = []
+                    for database in self._database_list:
+                        con = sqlite3.connect(database)
+                        self._all_connections.append(con)
+                    self._all_connections_established = True
+                self._conn = self._all_connections[indices[1]]
+            if indices[1] != self._current_database:
+                self._conn = self._all_connections[indices[1]]
+                self._current_database = indices[1]
+        return self
+
+    # @TODO: Is it necessary to return anything here?
+    def _close_connection(self) -> "CustomSQLiteDataset":
+        """Make sure that no sqlite3 connection is open.
+
+        This is necessary to calls this before passing to
+        `torch.DataLoader` such that the dataset replica on each worker
+        is required to create its own connection (thereby avoiding
+        `sqlite3.DatabaseError: database disk image is malformed` errors
+        due to inability to use sqlite3 connection accross processes.
+        """
+        if self._conn is not None:
+            self._conn.close()
+            del self._conn
+            self._conn = None
+        if self._database_list is not None:
+            if self._all_connections_established:
+                for con in self._all_connections:
+                    con.close()
+                del self._all_connections
+                self._all_connections_established = False
+                self._conn = None
+        return self
