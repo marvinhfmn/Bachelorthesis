2fedd713e8db28e1589a00b94d542e87edf8e8cd
Marvin Hoffmann <marvin.c.hoffmann@gmail.com>
2024-07-04 14:27:04 +0200

    added a custom Training label that computes the deposited energy on the fly, added some type hints, added method to get selections indices based on a given classification, added method to Create  a dataset when the training label is not in the database (add_label()), added Custom IceCube detector class

--- programming_GNNs_and_training/CustomAdditionsforGNNTraining.py
+++ programming_GNNs_and_training/CustomAdditionsforGNNTraining.py
@@ -1,9 +1,10 @@
 from typing import Dict, List, Optional, Tuple, Union, Callable
-
+import os
 import numpy as np
 import pandas as pd
 import sqlite3 as sql
 import torch
+from torch_geometric.data import Data
 from glob import glob
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
@@ -11,7 +12,40 @@ from graphnet.data.dataset import Dataset, SQLiteDataset, ParquetDataset, Ensemb
 from graphnet.utilities.logging import Logger
 from graphnet.models.graphs import GraphDefinition
 from graphnet.training.utils import make_dataloader
+from graphnet.training.labels import Label
+from graphnet.models.detector.detector import Detector
+from graphnet.constants import ICECUBE_GEOMETRY_TABLE_DIR
+from CustomSQL_dataset import CustomSQLiteDataset 
 
+
+class CustomLabel_depoEnergy(Label):
+    """Class for producing my label."""
+    def __init__(
+            self,
+            key: str = 'deposited_energy',
+            first_vertex_energy: str = 'first_vertex_energy', 
+            second_vertex_energy: str = 'second_vertex_energy', 
+            third_vertex_energy: str = 'third_vertex_energy', 
+            visible_track_energy: str = 'visible_track_energy',
+            visible_spur_energy: str = 'visible_spur_energy',
+            ):
+        """Construct `deposited_energy` label."""
+        self._firstve = first_vertex_energy
+        self._secondve = second_vertex_energy
+        self._thirdve = third_vertex_energy
+        self._vte = visible_track_energy
+        self._vse = visible_spur_energy
+
+        # Base class constructor
+        super().__init__(key=key)
+
+    def __call__(self, graph: Data) -> torch.tensor:
+        """Compute label for `graph`."""
+        vertex_energies = torch.nan_to_num(graph[self._firstve]) + torch.nan_to_num(graph[self._secondve]) + torch.nan_to_num(graph[self._thirdve])
+        tracks_and_spurs = torch.nan_to_num(graph[self._vte]) + torch.nan_to_num(graph[self._vse])
+        deposited_energy = vertex_energies+tracks_and_spurs
+        return deposited_energy
+    
 def make_train_validation_test_dataloader(
     db: str,
     graph_definition: GraphDefinition,
@@ -136,7 +170,11 @@ def make_train_validation_test_dataloader(
         test_dataloader,
     )
 
-def DataloaderSelection(db, value = 1.0, column = 'classification'):
+def DataloaderSelection(
+          db: Union[str, List[str]], 
+          value: float = 1.0, 
+          column: str = 'classification'
+          ):
     """
     Selects indices where the column fulfills the specified value. In case of column = 'classification and value = 1.0 throughgoing tracks would be selected.
     Returns the database filepaths and the selection indices in that order
@@ -158,7 +196,10 @@ def DataloaderSelection(db, value = 1.0, column = 'classification'):
     selection_indices = [ids for [path, ids] in indices]
     return dbfilepaths, selection_indices
 
-def GetSelectionIndicesExcludeNULL(db:  Union[str, List[str]], column: str):
+def GetSelectionIndicesExcludeNULL(
+          db:  Union[str, List[str]], 
+          column: str
+          ):
     """
     Selects events from db that are not NULL and returns database filepaths and the selection indices in that order
     """
@@ -203,8 +244,8 @@ def GetSelectionIndicesExcludeNULLandZERO(
 
 def GetSelectionIndicesExcludebelowThreshold(
           db:  Union[str, List[str]], 
-          column: Union[str, List[str]], 
-          threshold: int = 10, #threshold energy in GeV
+          column: str, 
+          threshold: int = 10, #threshold energy in units of db entries
           ):
     """
     Selects events from db that are above a certain threshold and returns database filepaths and the selection indices, in that order.
@@ -232,9 +273,90 @@ def GetSelectionIndicesExcludebelowThreshold(
     return dbfilepaths, selection_indices
 
 
+def GetSelectionIndices_basedonClassification(
+        db:  Union[str, List[str]], 
+        classifications: Union[int, List[int]] = [8, 9, 19, 20, 22, 23, 26, 27], 
+        deposited_energy_cols: Union[str, List[str]] = ["first_vertex_energy", "second_vertex_energy", "third_vertex_energy", "visible_track_energy", "visible_spur_energy"],
+        threshold: float = 10.0, #threshold energy in units of db entries
+        )-> Tuple[List[str], List[List[int]]]:
+    """
+    Get the indices of events with a specific classification and where the sum of specified deposition energies is above a threshold.
+    
+    Parameters:
+    db (Union[str, List[str]]): Database file name or list of database file names.
+    classifications (Union[int, List[int]]): Either a single classification or a list of classifications.
+                                             If -1, no filter based on classification is applied.
+    deposited_energy_cols (Union[str, List[str]]): List of column names for vertex energies.
+    threshold (float): Threshold for the sum of vertex energies.
+    
+    Returns:
+    Tuple[List[str], List[List[int]]]: Tuple containing:
+                                       - List of database file names.
+                                       - List of lists of event numbers that match the criteria.
+    """
+
+    if isinstance(db, str):
+        db = [db]
+
+    if classifications == -1:
+        classification_condition = "1=1"       
+    else:
+        if isinstance(classifications, int):
+            classifications = [classifications]
+        classification_condition = f"classification IN ({', '.join(map(str, classifications))})"
+    
+    if isinstance(deposited_energy_cols, str):
+        deposited_energy_cols = [deposited_energy_cols]
+    deposited_energy_str = " + ".join([f"IFNULL({col}, 0)" for col in deposited_energy_cols])
+
+    result  = []
+
+    for database in db:
+        with sql.connect(database) as con:
+            cur = con.cursor()
+            query = f"""
+            SELECT event_no 
+            FROM truth 
+            WHERE {classification_condition} 
+            AND ({deposited_energy_str}) > ?
+            """
+            ids = cur.execute(query, (threshold,)).fetchall()
+            selected_event_nos = [id[0] for id in ids]
+
+            result.append((database, selected_event_nos))
+    
+    databases, indices = zip(*result) if result else ([], [])
+    return list(databases), list(indices)
+
+def GetTrainingLabelEntries(
+          db:  Union[str, List[str]], 
+          column: str = 'first_vertex_energy', 
+          threshold: int = 10, #threshold energy in units of db entries
+          ) -> List[float]:
+    """
+    Selects values of column from the db where they are above a certain threshold and returns a list of these values.
+    """
+    energy_values = []
+    
+    # Ensure that db is a list
+    if isinstance(db, str):
+        db = [db]
+    
+    for dbfile in db:
+        con = sql.connect(dbfile)
+        cur = con.cursor()
+        query = f"SELECT {column} FROM truth WHERE {column} > {threshold}"
+        cur.execute(query)
+        values = cur.fetchall()
+        energy_values += [float(value[0]) for value in values]
+        con.close()
+    
+    return energy_values
+     
 #Idea: Create a custom dataset as e.g. a subdataset where the classification is specified and 
 # the data is split into train, val and test sets
-def CreateCustomDatasets(path: Union[str, List[str]], 
+def CreateCustomDatasets_traininglabelinDataset(
+                        path: Union[str, List[str]], 
                         graph_definition, 
                         features: List[str], 
                         truth: Union[str, List[str]], 
@@ -243,14 +365,19 @@ def CreateCustomDatasets(path: Union[str, List[str]],
                         truth_table: str = 'truth',
                         test_size: float = 0.1,
                         random_state = 42,
+                        logger: Logger = None,
                         setThreshold: bool = True, 
                         threshold: int = 10,
                         INCLUDEZEROS: bool = False, #'0' entries in databases are abandoned by default
                         INCLUDENULL: bool = False, #NULL entries in databases are abandoned by default
-                        ):
-    """Creates a custom Dataset, which can be reproduced due to the random_state variable. Although I am not sure about the event distribution inside these  datasets"""
-      
-    logger = Logger()
+                        ) -> Tuple[EnsembleDataset, EnsembleDataset, EnsembleDataset]:
+    """
+    Creates a custom Dataset, which can be reproduced due to the random_state variable. Although I am not sure about the event distribution inside these datasets
+    Only works if training label is in database. Method not applicable if you want to use a custom label, that is calculated on the fly using GraphNet.
+    """
+    if logger == None:
+        logger = Logger()
+    
     if(INCLUDENULL):
         # logger.info(f"Jokes on you, there is no functionality in setting INCLUDENULL to {INCLUDENULL}, except you don't get a dataset. Nice.")
         raise Exception(f"Jokes on you, there is no functionality in setting INCLUDENULL to {INCLUDENULL}, except you don't get a dataset. Nice.")
@@ -259,6 +386,9 @@ def CreateCustomDatasets(path: Union[str, List[str]],
     if isinstance(truth, str):
         truth = [truth]
         #truth has to be a list or tupel for line 283 in dataset.py: assert isinstance method to not fail
+    if isinstance(training_target_label, list):
+        training_target_label = training_target_label[0]
+        logger.info(f"Currently only one training label supported. Therefore choosing the first entry in provided list: {training_target_label}")
 
     common_kwargs = dict(
                 pulsemaps=pulsemap,
@@ -268,7 +398,6 @@ def CreateCustomDatasets(path: Union[str, List[str]],
                 graph_definition=graph_definition,
             )
     
-
     if isinstance(path, str) or (isinstance(path, List)):
         if isinstance(path, str):
             logger.info("One dataset path detected.")
@@ -308,8 +437,6 @@ def CreateCustomDatasets(path: Union[str, List[str]],
         validation_ensemble = EnsembleDataset(validationdatasets)
         testing_ensemble = EnsembleDataset(testingdatasets)
 
-        # print(training_ensemble, validation_ensemble, testing_ensemble)
-
         return training_ensemble, validation_ensemble, testing_ensemble
     
     else:
@@ -318,6 +445,117 @@ def CreateCustomDatasets(path: Union[str, List[str]],
     
     #save dataset configuration doesn't work for ensembleDatasets
     # dataset.config.dump(f"savedDatasets/{datasetname}.yml")
+
+def CreateCustomDatasets_CustomTrainingLabel(
+                        path: Union[str, List[str]], 
+                        graph_definition, 
+                        features: List[str], 
+                        truth: Union[str, List[str]], 
+                        classifications: Union[int, List[int]] = [8, 9, 19, 20, 22, 23, 26, 27],
+                        training_target_label: Label = CustomLabel_depoEnergy(),
+                        deposited_energy_cols: List[str] = ["first_vertex_energy", "second_vertex_energy", "third_vertex_energy", "visible_track_energy", "visible_spur_energy"],
+                        pulsemap: Union[str, List[str]] = 'InIceDSTPulses', 
+                        truth_table: str = 'truth',
+                        test_size: float = 0.1,
+                        random_state = 42,
+                        test_truth: Union[str, List[str]] = [],
+                        logger: Logger = None, 
+                        threshold: int = 10,
+                        ) -> Tuple[EnsembleDataset, EnsembleDataset, EnsembleDataset]:
+    """
+    Creates custom training, validation, and testing datasets from SQLite databases.
+
+    Parameters:
+    - path: File path or list of file paths to SQLite database(s) containing data.
+    - graph_definition: Definition of graph structure.
+    - features: List of features to include in the datasets.
+    - truth: List of event level truths to add to the graph of all datasets (train, val and test).
+    - classifications: List of classification IDs to filter events by. If -1, no filter is applied based on classification.
+    - training_target_label: Target label or feature to train on (e.g., energy deposition).
+    - deposited_energy_cols: List of columns representing deposited energy to consider for thresholding.
+    - pulsemap: Name or list of names of pulsemap tables in the databases.
+    - truth_table: Table name in the databases containing truth information.
+    - test_size: Proportion of data to reserve for testing.
+    - random_state: Seed for random number generator to ensure reproducibility.
+    - test_truth: Optional List of event level truths to add to the graph of only the testing dataset. list elements should be exclusive (no overlap with truth)
+    - logger: Optional logger object for logging messages.
+    - threshold: Threshold value for deposited energy columns to filter events.
+
+    Returns:
+    - Tuple containing three EnsembleDataset objects:
+        - Training dataset.
+        - Validation dataset.
+        - Testing dataset.
+    """
+    if logger == None:
+        logger = Logger()
+
+    #Assures truth (or training parameter) is a list
+    if isinstance(truth, str):
+        truth = [truth]
+    #truth has to be a list or tupel for line 283 in dataset.py: assert isinstance method to not fail
+    if isinstance(test_truth, str):
+        test_truth = [test_truth]
+
+    trainval_kwargs = dict(
+                pulsemaps=pulsemap,
+                features=features,
+                truth=truth, 
+                truth_table=truth_table,
+                graph_definition=graph_definition,
+            )
+    test_kwargs = dict(
+                pulsemaps=pulsemap,
+                features=features,
+                truth=truth + test_truth, 
+                truth_table=truth_table,
+                graph_definition=graph_definition,
+            )
+    
+    if isinstance(path, str) or (isinstance(path, List)):
+        if isinstance(path, str):
+            logger.info("One dataset path detected.")
+            path = [path] # enumerate(path) would otherwise lead to the string splitting into single characters
+        elif path and all(isinstance(s, str) for s in path):
+            logger.info("Multiple dataset paths detected")
+        else: 
+            logger.info("Path is an empty list ... Execution terminated")
+            return
+        
+    trainingdatasets = []
+    validationdatasets = []
+    testingdatasets = []
+
+    #splitting into train, val, test
+    for singledataset in path:
+        #Get indices where classification fulfills requirements           
+        database, VALIDINDICES = GetSelectionIndices_basedonClassification(
+                                            db=singledataset, 
+                                            classifications=classifications, 
+                                            deposited_energy_cols=deposited_energy_cols,
+                                            threshold=threshold
+                                            )
+        VALIDINDICES = VALIDINDICES[0]
+
+        trainval_selection, test_selection = train_test_split(
+        VALIDINDICES, test_size=test_size, random_state=random_state
+        )
+        training_selection, validation_selection = train_test_split(
+        trainval_selection, test_size=test_size, random_state=random_state
+        )
+        training_dataset = CustomSQLiteDataset(path=singledataset, **trainval_kwargs, selection=training_selection)
+        training_dataset.add_label(training_target_label)
+        trainingdatasets.append(training_dataset)
+
+        validation_dataset = CustomSQLiteDataset(path=singledataset, **trainval_kwargs, selection=validation_selection)
+        validation_dataset.add_label(training_target_label)
+        validationdatasets.append(validation_dataset)
+
+        testing_dataset = CustomSQLiteDataset(path=singledataset, **test_kwargs, selection=test_selection)
+        testing_dataset.add_label(training_target_label)
+        testingdatasets.append(testing_dataset)
+
+    return EnsembleDataset(trainingdatasets), EnsembleDataset(validationdatasets), EnsembleDataset(testingdatasets) 
 
 
 def HandleZeros(x):
@@ -335,7 +573,40 @@ def HandleZeros(x):
                 result[~zero_mask] = torch.log10(x[~zero_mask]) #apply log10 to non-zero elements
                 return result
     
-def CustomDetectorIceCube():
-     return
+class CustomIceCube86(Detector):
+    """Custom `Detector` class for IceCube without pmt area."""
+
+    geometry_table_path = os.path.join(
+        ICECUBE_GEOMETRY_TABLE_DIR, "icecube86.parquet"
+    )
+    xyz = ["dom_x", "dom_y", "dom_z"]
+    string_id_column = "string"
+    sensor_id_column = "sensor_id"
+
+    def feature_map(self) -> Dict[str, Callable]:
+        """Map standardization functions to each dimension of input data."""
+        feature_map = {
+            "dom_x": self._dom_xyz,
+            "dom_y": self._dom_xyz,
+            "dom_z": self._dom_xyz,
+            "dom_time": self._dom_time,
+            "charge": self._charge,
+            "rde": self._rde,
+            "hlc": self._identity,
+        }
+        return feature_map
+
+    def _dom_xyz(self, x: torch.tensor) -> torch.tensor:
+        return x / 500.0
+
+    def _dom_time(self, x: torch.tensor) -> torch.tensor:
+        return (x - 1.0e04) / 3.0e4
+
+    def _charge(self, x: torch.tensor) -> torch.tensor:
+        return torch.log10(x)
+
+    def _rde(self, x: torch.tensor) -> torch.tensor:
+        return (x - 1.25) / 0.25
+
 
 # if __name__ == "__main__":
